aadf9fc3f8eb9bf1048d8226eabfa1d0d836d66a
Varmetek <XDMAN500@users.noreply.github.com>
2024-11-03 18:15:04 -0500

    convert to gradle (#1)
    * Convert to gra
    
    * Clean up files

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    application
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "dev.fumaz.particlecreator.Main"
+}
+
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+}
